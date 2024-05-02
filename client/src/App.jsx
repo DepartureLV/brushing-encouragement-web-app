122cc0f3ceae9ff748c21e3fc2f920d4c1fd2ef0
Ning Chang <chaning55@gmail.com>
2024-05-02 02:53:04 -0700

    feat: components to read streak score and star score

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react"
+import "./App.css"
+import Streak from "./Streak"
+import Star from "./Star"
 
 function App() {
-  const [count, setCount] = useState(0)
+// Use States
+  const [streak, setStreak] = useState(0);
+  const [star, setStar] = useState(0);
 
+// Use Effects
+  useEffect = (() => {
+    getStreak();
+    getStar();
+  }, []);
+
+// Handle functions
+  // async function getStreak() {
+  //   const streak = await fetch('endpoint');//endpoint to get from score table column streak_score where id =user_id
+  //   setStreak(streak);
+  // }
+
+  // async function getStar() {
+  //   const star = await fetch('endpoint');//endpoint to get from score table column star_score where id =user_id
+  //   setStar(star);
+  // }
+
+// Return
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
+      <Streak className="streak" streakScore={streak}/>
+      <Star className="star" starScore={star}/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
