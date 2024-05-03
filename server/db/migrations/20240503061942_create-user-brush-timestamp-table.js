d14b6be434c25d59da53c76563602e1c97a0c98a
Musashi117 <32604297+Musashi117@users.noreply.github.com>
2024-05-03 16:01:59 +0900

    migration: create user brush timestamp table

---
+++ server/db/migrations/20240503061942_create-user-brush-timestamp-table.js
@@ -0,0 +1,24 @@
+const { USER_BRUSH_TIMESTAMP_TABLE } = require("../../src/global/global");
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable(USER_BRUSH_TIMESTAMP_TABLE, (table) => {
+    table.integer('user_id').notNullable();
+    table.integer('brush_id').notNullable();
+
+    table.unique(['user_id', 'brush_id'], {
+      indexName: 'idx_user_brush_timestamp_id',
+    });
+  });
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable(USER_BRUSH_TIMESTAMP_TABLE);
+};
