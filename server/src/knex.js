a5a404f209e0b25ae2acda26f49d4f85f0011419
Musashi117 <32604297+Musashi117@users.noreply.github.com>
2024-05-03 09:08:04 +0900

    chore: update dependencies using dotenv

--- server/src/knex.js
+++ server/src/knex.js
@@ -2,5 +2,4 @@ const config = require('./../knexfile');
 const knex = require('knex');
 
 const environement = (process.env.NODE_ENV && process.env.NODE_ENV === "production") ? "production" : "development";
-
 module.exports = knex(config[environement]);
