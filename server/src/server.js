b8e7cccb87871ab723ca81eaa0ffe1f9c8678918
Musashi117 <32604297+Musashi117@users.noreply.github.com>
2024-05-03 14:29:08 +0900

    feat: create landing endpoint

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
+const { setupServer } = require("./endpoints/endpoints");
 
+const server = setupServer();
 const PORT = parseInt(process.env.PORT) || 8080;
 
-app.use(express.json());
-app.use(cors());
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
 })
+
