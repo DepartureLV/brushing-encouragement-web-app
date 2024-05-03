658a2744a08a21071f2ec8b35eab4752206baea0
Musashi117 <32604297+Musashi117@users.noreply.github.com>
2024-05-03 10:58:15 +0900

    feat: seed user_credentials for testing

--- server/src/authentication/password-hasher.js
+++ server/src/authentication/password-hasher.js
@@ -1,7 +1,11 @@
 const crypto = require('crypto');
 
+function generateSalt () {
+  return crypto.randomBytes(6).toString('hex');
+}
+
 function generateHashedPassword (plainTextPassword, saltHex = null) {
-  const salt = saltHex || crypto.randomBytes(6).toString('hex');
+  const salt = saltHex || generateSalt();
   const saltAndPassword = `${salt}${plainTextPassword}`
   const hash = crypto.createHash('sha256');
   const hashedPassword = hash.update(saltAndPassword).digest('hex');
@@ -9,5 +13,6 @@ function generateHashedPassword (plainTextPassword, saltHex = null) {
 }
 
 module.exports = {
+  generateSalt,
   generateHashedPassword
 }
--- server/src/test/authentication/password-hasher-spec.js
+++ server/src/test/authentication/password-hasher-spec.js
@@ -1,23 +1,34 @@
 const {expect} = require('chai');
 const crypto = require ('crypto');
-const { generateHashedPassword } = require('../../authentication/password-hasher.js');
+const { generateSalt, generateHashedPassword } = require('../../authentication/password-hasher.js');
 
 
 describe('Generating a hashed password', () => {
-  const salt = crypto.randomBytes(6).toString('hex');
+  const salt = generateSalt();
   const plainPassword = 'HelloWorld'
+
+  describe('generateSalt', () => {
+    it('should return a string', () => {
+      expect(generateSalt()).to.be.a('string');
+    })
+  })
 
   describe('generateHashedPassword', () => {
     it ('should return a string', () => {
       expect(generateHashedPassword(plainPassword)).to.be.a('string');
     })
+    
     it ('should return the same hashed string if the salt and plain text password are the same', () => {
-      expect(generateHashedPassword(plainPassword, salt));
+      const expectedPlainPassword = plainPassword;
+      const expectedHashedPassword = generateHashedPassword(expectedPlainPassword, salt);
+      expect(generateHashedPassword(plainPassword, salt)).to.equal(expectedHashedPassword);
     })
+
     it ('should return a different string if salt is different', () => {
       const tempSalt = crypto.randomBytes(6).toString('hex');
       const diffHashedPassword = generateHashedPassword(plainPassword, tempSalt);
       expect(generateHashedPassword(plainPassword, salt)).to.not.equal(diffHashedPassword);
     })
+
   })
 })
