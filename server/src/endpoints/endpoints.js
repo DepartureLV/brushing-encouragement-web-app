ffeb5e8dfc2c917a4a210d4777a8a93d9e64afd6
Ning Chang <chaning55@gmail.com>
2024-05-04 20:19:58 -0700

    feat: server endpoint sending scores from db

--- server/src/endpoints/endpoints.js
+++ server/src/endpoints/endpoints.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const knex = require('../knex');
 
 app.use(express.json());
 app.use(cors());
@@ -10,6 +11,16 @@ const setupServer = () => {
     res.status(200).send("Welcome to Brush Buddy");
   })
 
+  app.get("/score/:id", async (req, res) => {
+    // let userId = req.body.id;
+    const userId = req.params.id;
+    let score = await knex("scores")
+      .join("user_credentials", "user_credentials.id", "=", "scores.user_id")
+      .select({streakScore: "streak_score", starScore: "star_score"})
+      .where({"user_credentials.id": userId});
+    console.log(score);
+    res.status(200).send(score);
+  })
   return app;
 }
 
