001bd18b00a7d78d321817b98937025fac13231d
dominik <dominik@shintakukanadenoMacBook-Air.local>
2024-05-08 10:55:29 +0900

    feat:add react router && basic layout

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-// USE REACT ROUTER DOM HERE
-// #########################
-// #########################
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/login",
+    element: <div>Make the login page!</div>,
+  },
+  {
+    path: "*",
+    element: <div>Make an error page!</div>,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
