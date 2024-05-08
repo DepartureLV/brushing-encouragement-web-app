18a9ec7df3e7fe4e96dc7bf857dc79f499e02bd8
Nattawat Pitikomon <nattawat.arch@gmail.com>
2024-05-08 17:05:19 +0900

    chore: rename table

--- server/db/seeds/004-flossy_timestamps.js
+++ server/db/seeds/004-flossy_timestamps.js
@@ -1,4 +1,4 @@
-const { FLOSSY_TABLE } = require("../../src/global/global");
+const { FLOSSY_TABLE_V2 } = require("../../src/global/global");
 
 /**
  * @param { import("knex").Knex } knex
@@ -6,10 +6,10 @@ const { FLOSSY_TABLE } = require("../../src/global/global");
  */
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex(FLOSSY_TABLE).del();
-  await knex(FLOSSY_TABLE).truncate();
-  await knex(FLOSSY_TABLE).insert([{ id: 1, user_id: 1 }]);
+  await knex(FLOSSY_TABLE_V2).del();
+  await knex(FLOSSY_TABLE_V2).truncate();
+  await knex(FLOSSY_TABLE_V2).insert([{ id: 1, user_id: 1 }]);
   await knex.raw(
-    `select setval(\'brush_timestamp_id_seq\', max(id)) from ${FLOSSY_TABLE}`
+    `select setval(\'brush_timestamp_id_seq\', max(id)) from ${FLOSSY_TABLE_V2}`
   );
 };
--- server/src/global/global.js
+++ server/src/global/global.js
@@ -4,6 +4,7 @@ const BRUSH_TIMESTAMP_TABLE = "brush_timestamp";
 const USER_BRUSH_TIMESTAMP_TABLE = "user_brush_timestamp";
 const BRUSH_TIMESTAMP_TABLE_V2 = "brush_timestamps";
 const FLOSSY_TABLE = "flossy";
+const FLOSSY_TABLE_V2 = "flossy_timestamps";
 
 module.exports = {
   USER_CREDENTIALS_TABLE,
@@ -12,4 +13,5 @@ module.exports = {
   USER_BRUSH_TIMESTAMP_TABLE,
   BRUSH_TIMESTAMP_TABLE_V2,
   FLOSSY_TABLE,
+  FLOSSY_TABLE_V2
 };
