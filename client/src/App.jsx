68c0b3de5f663ad929816014b1fd91441f0f0b61
dominik <dominik@shintakukanadenoMacBook-Air.local>
2024-05-08 15:11:45 +0900

    feat: add MVP leaderboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Timer from "./components/Timer";
+import Leaderboard from "./components/Leaderboard";
 
 function App() {
   return (
     <>
       <Timer/>
+      <Leaderboard/>
     </>
   )
 }
